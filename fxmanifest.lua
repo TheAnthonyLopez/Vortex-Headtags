1640384f3f04f8f454b33d4b06c3ef0cac0f7b23
TheAnthonyLopez <anthonya46048@gmail.com>
2025-04-23 08:07:27 -0400

    fxmanifest.lua complete.

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Your Name'
+description 'Standalone Headtag and Gangtag System'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'ox_lib'
+}
