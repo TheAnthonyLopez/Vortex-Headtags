b27722092645c806030dd60f58326d092473ffc9
TheAnthonyLopez <anthonya46048@gmail.com>
2025-04-23 07:54:31 -0400

    config.lua fixed.

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
-Config.Tags {
+Config = {}
+
+Config.Tags = {
     {ace = "tag.owner", headtag = "~p~Owner", gangtag = "~p~Owner"},
     {ace = "tag.coowner", headtag = "~b~Co-Owner", gangtag = "~b~Co-Owner"},
     {ace = "tag.admin", headtag = "~g~Admin", gangtag = "~g~Admin"},
 }
-
- -- More coming soon...
